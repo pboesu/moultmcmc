718548e3854218412181adfaae227e6750f34139
Philipp Boersch-Supan <philipp@boersch-supan.de>
2020-09-29 00:00:42 +0100

    linear predictor on duration added

--- R/uz1_linpred.R
+++ R/uz1_linpred.R
@@ -1,20 +1,22 @@
 #' Bayesian inference for the Type 1 moult model
 #'
 #' @export
-#' @param formula model formula for start date
+#' @param start_formula model formula for start date
+#' @param duration_formula model formula for duration
 #' @param data Input data frame must contain a numeric column "date" and a column "moult_cat" which is a numeric vector of categorical moult codes (1 = old plumage,2 = moulting,3 = new plumage).
 #' @param init Specification of initial values for all or some parameters. Can be the string "auto" for an automatic guess based on the data, or any of the permitted rstan options: the digit 0, the strings "0" or "random", or a function. See the detailed documentation for the init argument in ?rstan::stan.
 #' @param ... Arguments passed to `rstan::sampling` (e.g. iter, chains).
 #' @return An object of class `stanfit` returned by `rstan::sampling`
 #'
 #TODO: implement an input data class which ensures column names and correct encoding for categorical variables
-uz1_linpred <- function(formula, data, init = "auto",...) {
+uz1_linpred <- function(start_formula, duration_formula, data, init = "auto",...) {
   stopifnot(all(data$moult_cat %in% c(1,2,3)))
 
   #order data by moult category
   data <- data[order(data$moult_cat),]
-  #setup model matrix
-  X_mu <- model.matrix(formula, data)
+  #setup model matrices
+  X_mu <- model.matrix(start_formula, data)
+  X_tau <- model.matrix(duration_formula, data)
   #prepare data structure for stan
   standata <- list(old_dates = data$date[data$moult_cat==1],
                    N_old = length(data$date[data$moult_cat==1]),
@@ -23,7 +25,9 @@ uz1_linpred <- function(formula, data, init = "auto",...) {
                    new_dates = data$date[data$moult_cat==3],
                    N_new = length(data$date[data$moult_cat==3]),
                    X_mu = X_mu,
-                   N_pred_mu = ncol(X_mu))
+                   N_pred_mu = ncol(X_mu),
+                   X_tau = X_tau,
+                   N_pred_tau = ncol(X_tau))
   #guess initial values
   if(init == "auto"){
     mu_start = mean(max(standata$old_dates),min(standata$moult_dates))
@@ -31,7 +35,9 @@ uz1_linpred <- function(formula, data, init = "auto",...) {
     sigma_start = min(10,sd(standata$moult_dates))
     initfunc <- function(chain_id = 1) {
       # cat("chain_id =", chain_id, "\n")
-      list(beta_mu = as.array(c(mu_start,rep(0, standata$N_pred_mu - 1))), tau = tau_start, sigma = sigma_start)
+      list(beta_mu = as.array(c(mu_start,rep(0, standata$N_pred_mu - 1))),
+           beta_tau = as.array(c(tau_start, rep(0, standata$N_pred_tau - 1))),
+           sigma = sigma_start)
     }
     out <- rstan::sampling(stanmodels$uz1_linpred, data = standata, init = initfunc, ...)
   } else {
--- inst/stan/uz1_linpred.stan
+++ inst/stan/uz1_linpred.stan
@@ -14,32 +14,42 @@ data {
   //predictors
   int N_pred_mu;//number of predictors for start date
   matrix[N_old+N_moult+N_new,N_pred_mu] X_mu;//design matrix for start date NB: when forming design matrix must paste together responses in blocks old, moult, new
+  int N_pred_tau;//number of predictors for duration
+  matrix[N_old+N_moult+N_new,N_pred_tau] X_tau;//design matrix for duration NB: when forming design matrix must paste together responses in blocks old, moult, new
 }
 
 // The parameters accepted by the model. Our model
 // accepts two parameters 'mu' and 'sigma'.
 parameters {
 
-  real<lower=0> tau;//moult duration
+  //real<lower=0> tau;//moult duration
   real<lower=0> sigma;//population varianmoult start date
   vector[N_pred_mu] beta_mu;//regression coefficients for start date
+  vector[N_pred_tau] beta_tau;//regression coefficients for duration
 }
 
-// The model to be estimated. We model the output
-// 'y' to be normally distributed with mean 'mu'
-// and standard deviation 'sigma'.
+// The model to be estimated.
 model {
   vector[N_old] P;
   vector[N_moult] Q;
   vector[N_new] R;
   vector[N_old+N_moult+N_new] mu;//start date lin pred
+  vector[N_old+N_moult+N_new] tau;//duration lin pred
 
   mu = X_mu * beta_mu;
+//  print(mu);
+  tau = X_tau * beta_tau;
+//  print(tau);
 
 for (i in 1:N_old) P[i] = 1 - Phi((old_dates[i] - mu[i])/sigma);
-for (i in 1:N_moult) Q[i] = Phi((moult_dates[i] - mu[i + N_old])/sigma) - Phi((moult_dates[i] - tau - mu[i + N_old])/sigma);
-for (i in 1:N_new) R[i] = Phi((new_dates[i] - tau - mu[i + N_old + N_moult])/sigma);
-
+//print(P);
+for (i in 1:N_moult) Q[i] = Phi((moult_dates[i] - mu[i + N_old])/sigma) - Phi((moult_dates[i] - tau[i + N_old] - mu[i + N_old])/sigma);
+//print(Q);
+for (i in 1:N_new) R[i] = Phi((new_dates[i] - tau[i + N_old + N_moult] - mu[i + N_old + N_moult])/sigma);
+//print(R);
+//print(sum(log(P)));
+//print(sum(log(Q)));
+//print(sum(log(R)));
 target += sum(log(P))+sum(log(Q))+sum(log(R));
 //priors
 //mu ~ normal(0,10);
