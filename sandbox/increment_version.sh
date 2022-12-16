222d1f506657985f9a8c0590136b626b37d85f08
Philipp Boersch-Supan <philipp.boerschsupan@bto.org>
2022-12-16 10:29:27 +0000

    update exports

--- sandbox/increment_version.sh
+++ sandbox/increment_version.sh
@@ -11,3 +11,4 @@ new_version="$current_tag.$((current_commit + 9000))" # results in 0.1.0.9040
 sed -i -- "s/^Version: .*/Version: ${new_version}/" DESCRIPTION
 echo "First 3 lines of DESCRIPTION:"
 head -3 DESCRIPTION
+git add DESCRIPTION
