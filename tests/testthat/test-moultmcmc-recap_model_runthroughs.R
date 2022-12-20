f0e97a4e3b41f28cba84b0fb5886ce4c92c6fc88
Philipp Boersch-Supan <philipp.boerschsupan@bto.org>
2022-12-20 17:57:50 +0000

    updated test for t2r mk2 - may segfault

--- tests/testthat/test-moultmcmc-recap_model_runthroughs.R
+++ tests/testthat/test-moultmcmc-recap_model_runthroughs.R
@@ -10,9 +10,9 @@ test_that("moultmcmc uz2_recap works", {
                   id_column = "id",
                   data = recaptures,
                   flat_prior = FALSE,
-                  type = 2,
+                  type = 2, #mk2 = TRUE,
                   log_lik = FALSE,
-                  chains = 2,
+                  chains = 1,
                   cores = 2,
                   control = list(adapt_delta = 0.99, max_treedepth = 11),
                   iter = 200,
@@ -20,10 +20,31 @@ test_that("moultmcmc uz2_recap works", {
                   open_progress = FALSE)
 expect_s3_class(m2r, "moultmcmc")
 
+
 as.data.frame(ranef(m2r)) %>% tibble::rownames_to_column('id') %>% left_join(recaptures %>% group_by(id) %>% summarize(start_date = unique(start_date), n_moult = sum(pfmg_sampled != 0 & pfmg_sampled != 1), n_total = n())) -> joined_df
 #ggplot(joined_df, aes(x = start_date-196.83, y = mean, ymin = `2.5%`, ymax = `97.5%`, col = factor(n_moult), pch = factor(n_total), label = id)) + geom_pointrange() + geom_text(col = 'black', nudge_y=0.1) + geom_abline(slope = 1, intercept = 0)
 
 })
+
+test_that("moultmcmc uz2_recap mk2 works", {
+  m2r2 = moultmcmc(moult_column = "pfmg_sampled",
+                   date_column = "date_sampled",
+                   id_column = "id",
+                   data = recaptures,
+                   flat_prior = FALSE,
+                   type = 2, mk2 = TRUE,
+                   log_lik = FALSE,
+                   chains = 1,
+                   cores = 2,
+                   control = list(adapt_delta = 0.99, max_treedepth = 11),
+                   iter = 200,
+                   refresh = 50,
+                   open_progress = FALSE)
+  expect_s3_class(m2r2, "moultmcmc")
+
+})
+
+
 test_that("moultmcmc uz2l_recap works", {
   uz2rl = moultmcmc(moult_column = "pfmg_sampled",
                     date_column = "date_sampled",
