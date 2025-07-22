fe46a53efa34227305a26287d5e4ccedfd53267e
Philipp Boersch-Supan <pboesu@gmail.com>
2025-07-22 17:12:16 +0100

    redocument

--- R/moultmcmc-package.R
+++ R/moultmcmc-package.R
@@ -2,7 +2,6 @@
 #'
 #' @description Precompiled Stan models to conduct full Bayesian inference in regression models for the phenology of avian primary moult based on the approach of Underhill & Zucchini (1988) and related hierarchical models to accommodate repeated-measures data (within-season recaptures of individuals).
 #'
-#' @docType package
 #' @name moultmcmc-package
 #' @useDynLib moultmcmc, .registration = TRUE
 #' @import methods
@@ -10,13 +9,13 @@
 #' @importFrom rstan sampling
 #'
 #' @references
-#' Boersch-Supan et al. (2022) Bayesian inference for models of moult duration and timing in birds arXiv DOI:10.48550/arXiv.2205.12120
+#' Boersch-Supan et al. (2024) Extended molt phenology models improve inferences about molt duration and timing. Ornithology DOI:10.1093/ornithology/ukae003
 #'
 #' Underhill & Zucchini (1988) A model for avian primary moult. Ibis 130:358 DOI:10.1111/j.1474-919X.1988.tb00993.x
 #'
 #' Stan Development Team (2022). RStan: the R interface to Stan. R package version 2.21.5. https://mc-stan.org
 #'
-NULL
+"_PACKAGE"
 
 ## usethis namespace: start
 #' @importFrom magrittr %>%
